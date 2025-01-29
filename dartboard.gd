a91262ba5cd8a5399dce5f81a498a1f9ff01d0c8
Duschmolll <65500704+Duschmolll@users.noreply.github.com>
2025-01-29 13:56:43 +0100

    Added some functions for btn

---
+++ dartboard.gd
@@ -0,0 +1,37 @@
+extends Control
+
+#Control Group Nodes
+@export var group00: Control
+@export var group01: Control
+@export var group02: Control
+@export var group03: Control
+@export var group04: Control
+@export var group05: Control
+@export var group06: Control
+@export var group07: Control
+@export var group08: Control
+@export var group09: Control
+@export var group10: Control
+@export var group11: Control
+@export var group12: Control
+@export var group13: Control
+@export var group14: Control
+@export var group15: Control
+@export var group16: Control
+@export var group17: Control
+@export var group18: Control
+@export var group19: Control
+@export var group20: Control
+
+#Variables
+var controlGroupList: Array
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	controlGroupList = [group00,group01,group02,group03,group04,group05,group06,group07,group08,group09,group10,group11,group12,group13,group14,group15,group16,group17,group18,group19,group20]
+	
+	for elem:Control in controlGroupList:
+		for btn in elem.get_children():
+			if btn.name != "Number":
+				btn.pressed.connect(Callable(self, "number_buttons").bind(btn)) 
